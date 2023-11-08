b389b0c6b8d0351c415c9aa122ea399898dceec1
BingBong <bingbong9981@gmail.com>
2023-11-08 21:27:09 +0900

    Feat : 그룹 리스트/추가 Activity 생성

---
+++ app/src/main/java/com/Apic/apic/Group_add.kt
@@ -0,0 +1,11 @@
+package com.Apic.apic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Group_add : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group_add)
+    }
+}
---
+++ app/src/main/java/com/Apic/apic/Group_list.kt
@@ -0,0 +1,11 @@
+package com.Apic.apic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Group_list : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group_list)
+    }
+}
