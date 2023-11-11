ac18bc084d635dd8937ed752c93b9756cd3db19f
19013na <19013@naver.com>
2023-11-11 20:20:09 +0900

    Feat : Bottom Navigation 생성 / fragment_friend, _calendar

--- app/src/main/java/com/Apic/apic/MainActivity.kt
+++ app/src/main/java/com/Apic/apic/MainActivity.kt
@@ -2,10 +2,38 @@ package com.Apic.apic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.Apic.apic.databinding.ActivityMainBinding
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    private val fragmentManager: FragmentManager = supportFragmentManager
+    private val fragmentCalendar = CalendarFragment()
+    private val fragmentFriend = FriendFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val transaction: FragmentTransaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.menu_frame_view, fragmentCalendar).commitAllowingStateLoss()
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigationview)
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            val transaction = fragmentManager.beginTransaction()
+
+            when (menuItem.itemId) {
+                R.id.menu_home -> transaction.replace(R.id.menu_frame_view, fragmentCalendar).commitAllowingStateLoss()
+                R.id.menu_friend -> transaction.replace(R.id.menu_frame_view, fragmentFriend).commitAllowingStateLoss()
+                //R.id.menu_share -> transaction.replace(R.id.menu_frame_view, fragmentShare).commitAllowingStateLoss()
+                // 위 코드는 아직 fragment 추가 안해서 주석 처리 합니다.
+            }
+
+            true
+        }
     }
+
 }
