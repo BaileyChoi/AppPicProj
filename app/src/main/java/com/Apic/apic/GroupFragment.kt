2c7baca3741dde279b31c252e0bc2c7d6eb3d464
narong <criticalpxint@naver.com>
2023-11-12 05:00:57 +0900

    Feat : Group -  section, tapbar  생성 /  DateFragment, AlbumFragment, GroupFragment, BoardFragment 생성

---
+++ app/src/main/java/com/Apic/apic/GroupFragment.kt
@@ -0,0 +1,77 @@
+package com.Apic.apic
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import java.lang.IllegalArgumentException
+class GroupFragment : Fragment() {
+
+    private lateinit var boardFragment: BoardFragment
+    private lateinit var memberFragment: MemberFragment
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_group, container, false)
+
+
+
+
+
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//
+//        val viewPager: ViewPager2 = view.findViewById(R.id.view_pager)
+//        val adapter = PagerAdapter(this)
+//        viewPager.adapter = adapter
+//
+//        val tabLayout: TabLayout = view.findViewById(R.id.tabs)
+//
+//        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+//            override fun onTabSelected(tab: TabLayout.Tab?) {
+//                val transaction = childFragmentManager.beginTransaction()
+//                when (tab?.text) {
+//                    "Tab1" -> transaction.replace(R.id.tab_content, BoardFragment())
+//                    "Tab2" -> transaction.replace(R.id.tab_content, FriendsFragment())
+//                }
+//                transaction.commit()
+//            }
+//
+//            override fun onTabUnselected(tab: TabLayout.Tab?) {
+//                // Unselected tab logic, if needed
+//            }
+//
+//            override fun onTabReselected(tab: TabLayout.Tab?) {
+//                // Reselected tab logic, if needed
+//            }
+//        })
+//
+
+
+    }
+
+    class PagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+        override fun getItemCount(): Int {
+            return 2
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> BoardFragment()
+                1 -> MemberFragment()
+                else -> throw IllegalArgumentException("Invalid position: $position")
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/Apic/apic/MainActivity.kt
+++ app/src/main/java/com/Apic/apic/MainActivity.kt
@@ -2,10 +2,56 @@ package com.Apic.apic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    private lateinit var tabLayout:TabLayout
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var adapter: FragmentPagerAdapter
+    override fun onCreate(savedInstanceState:
+                          Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_group)
+
+        tabLayout = findViewById(R.id.tab_layout)
+        viewPager2 = findViewById(R.id.viewPager2)
+
+        adapter = FragmentPagerAdapter(supportFragmentManager, lifecycle)
+
+        tabLayout.addTab(tabLayout.newTab().setText("Board"))
+        tabLayout.addTab(tabLayout.newTab().setText("Friends"))
+
+        viewPager2.adapter = adapter
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab!=null) {
+                    viewPager2.currentItem = tab.position
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                if (tab!=null) {
+                    viewPager2.currentItem = tab.position
+                }
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                if (tab!=null) {
+                    viewPager2.currentItem = tab.position
+                }
+            }
+
+        })
+
+        viewPager2.registerOnPageChangeCallback(object: OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        } )
     }
 }
