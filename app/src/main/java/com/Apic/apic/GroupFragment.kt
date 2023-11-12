730d92489e64db9fd3ef89347df8ef0a5c11e659
narong <criticalpxint@naver.com>
2023-11-12 22:00:18 +0900

    Remove : AlbumFragment, DateFragment, fragment_album.xml, fragment_date.xml 삭제

--- app/src/main/java/com/Apic/apic/GroupFragment.kt
+++ app/src/main/java/com/Apic/apic/GroupFragment.kt
@@ -20,58 +20,14 @@ class GroupFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_group, container, false)
 
 
-
-
-
-
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//
-//        val viewPager: ViewPager2 = view.findViewById(R.id.view_pager)
-//        val adapter = PagerAdapter(this)
-//        viewPager.adapter = adapter
-//
-//        val tabLayout: TabLayout = view.findViewById(R.id.tabs)
-//
-//        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-//            override fun onTabSelected(tab: TabLayout.Tab?) {
-//                val transaction = childFragmentManager.beginTransaction()
-//                when (tab?.text) {
-//                    "Tab1" -> transaction.replace(R.id.tab_content, BoardFragment())
-//                    "Tab2" -> transaction.replace(R.id.tab_content, FriendsFragment())
-//                }
-//                transaction.commit()
-//            }
-//
-//            override fun onTabUnselected(tab: TabLayout.Tab?) {
-//                // Unselected tab logic, if needed
-//            }
-//
-//            override fun onTabReselected(tab: TabLayout.Tab?) {
-//                // Reselected tab logic, if needed
-//            }
-//        })
-//
 
 
     }
 
-    class PagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
 
-        override fun getItemCount(): Int {
-            return 2
-        }
-
-        override fun createFragment(position: Int): Fragment {
-            return when (position) {
-                0 -> FirstFragment()
-                1 -> SecondFragment()
-                else -> throw IllegalArgumentException("Invalid position: $position")
-            }
-        }
-    }
 
 }
