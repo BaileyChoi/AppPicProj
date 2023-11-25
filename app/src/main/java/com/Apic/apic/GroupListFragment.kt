63fdb3cbdca579c8bbad5b1713a03fa85d6b6716
BingBong <bingbong9981@gmail.com>
2023-11-25 18:37:07 +0900

    Fix : GroupListFragment 오류 해결

--- app/src/main/java/com/Apic/apic/GroupListFragment.kt
+++ app/src/main/java/com/Apic/apic/GroupListFragment.kt
@@ -8,15 +8,11 @@ import android.util.Log
 import android.view.*
 import android.widget.EditText
 import android.widget.Toast
-import androidx.compose.ui.text.toLowerCase
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.Apic.apic.databinding.FragmentGroupListBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.firestore.FirebaseFirestore
 
 // TODO: Rename parameter arguments, choose names that match
