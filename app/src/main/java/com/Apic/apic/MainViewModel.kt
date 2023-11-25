3f4290af7f7556951cfb3bc2c2c5cd436b193a67
BingBong <bingbong9981@gmail.com>
2023-11-26 01:17:50 +0900

    Fix : 불필요한 코드 삭제

--- app/src/main/java/com/Apic/apic/MainViewModel.kt
+++ app/src/main/java/com/Apic/apic/MainViewModel.kt
@@ -9,7 +9,6 @@ import com.Apic.apic.data.Todo
 import com.Apic.apic.data.TodoDatabase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
